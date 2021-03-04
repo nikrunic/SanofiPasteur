7cda4c71c71d7e61ed90518d499aa3fc6ef3926a
krunal prajapati <krunalcodemaster@gmail.com>
2021-03-05 00:45:34 +0530

    new ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,95 @@
-$(function() {
-    if ($(window).width() < 769) {
-        $(".bottomHeader").css({ top: $('.topHeader').outerHeight(true) });
-    }
- 
-    $(".tab_title_inner_blk li").on("click", function(e) {
-        changeTab($(this));
-    });
+$(function () {
+  if ($(window).width() < 769) {
+    $(".bottomHeader").css({ top: $(".topHeader").outerHeight(true) });
+  }
 
-    if (window.location.hash) {
-        var hash = window.location.hash;
-        $(".tab_title_inner_blk li span a[href='" + hash + "']").parents("li").click();
-    }
-   
-    if ($('.scroll').length) {
-        $('.scroll').jScrollPane();
-    }
+  $(".tab_title_inner_blk li").on("click", function (e) {
+    changeTab($(this));
+  });
 
+  if (window.location.hash) {
+    var hash = window.location.hash;
+    $(".tab_title_inner_blk li span a[href='" + hash + "']")
+      .parents("li")
+      .click();
+  }
+
+  if ($(".scroll").length) {
+    $(".scroll").jScrollPane();
+  }
+  $("body").on("click", ".listigHeader", function (e) {
+    $(".box-a1").find(".level-box").stop().slideUp();
+    $(this).closest(".box-a1").find(".level-box").stop().slideToggle();
+  });
 });
 
-$(window).resize(function() {
-    if ($('.scroll').length) {
-        $('.scroll').jScrollPane();
-    }
-    // var bannerHeight = $(window).height() - $("header").height();
-    // $(".banner_blk").height(bannerHeight);
+$(window).resize(function () {
+  if ($(".scroll").length) {
+    $(".scroll").jScrollPane();
+  }
+  // var bannerHeight = $(window).height() - $("header").height();
+  // $(".banner_blk").height(bannerHeight);
 });
 
 function changeTab(el) {
-    el.parents(".tab_blk").find(".tab_title_inner_blk li").removeClass("active");
-    el.addClass("active");
-    var index = el.parents(".tab_blk").find(".tab_title_inner_blk li").index(el);
-    el.parents(".tab_blk").find(".tab_cont_li").removeClass("active").eq(index).addClass("active");
-    // console.log(index);
+  el.parents(".tab_blk").find(".tab_title_inner_blk li").removeClass("active");
+  el.addClass("active");
+  var index = el.parents(".tab_blk").find(".tab_title_inner_blk li").index(el);
+  el.parents(".tab_blk")
+    .find(".tab_cont_li")
+    .removeClass("active")
+    .eq(index)
+    .addClass("active");
+  // console.log(index);
+}
+
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+let map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
